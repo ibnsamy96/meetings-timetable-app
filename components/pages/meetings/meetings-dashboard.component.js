ebea38e70fa4755dc439c5e80a468b1d33a92c5e
Mahmoud Samy <mahmoudsamy50@outlook.com>
2021-01-17 18:23:04 +0200

    added todo to create responsive columns

--- components/pages/meetings/meetings-dashboard.component.js
+++ components/pages/meetings/meetings-dashboard.component.js
@@ -1,7 +1,23 @@
 export const MeetingsDashboardComponent = {
-    render: () => {
-        return `
+  render: () => {
+    return `
 
         `
-    }
+  }
 }
+
+/*
+
+TODO 
+تحديد عدد الأعمدة حسب مقاس الصفحة
+
+<div class="container">
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+</div>
+
+*/
