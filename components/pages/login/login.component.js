a264976f3a35e1366ae9050d1aef9f66b8870b06
Mahmoud Samy <mahmoudsamy50@outlook.com>
2021-01-24 08:34:23 +0200

    removed unnecessary padding and margin

--- components/pages/login/login.component.js
+++ components/pages/login/login.component.js
@@ -5,7 +5,7 @@ import {
 export const LoginComponent = {
     render: () => {
         return `
-        <section id='login-component' class=" text-center d-flex justify-content-center align-items-center mt-4 pb-4">
+        <section id='login-component' class=" text-center d-flex justify-content-center align-items-center mt-4">
         <div>
             <h1 class="h2 mb-3 pageHeadingText"> أهلًا بيك صديقي الليدر المعوي العزيز <i
                     class="h4 fw-bolder fas fa-heart" style="color: #e85a71;"></i>
@@ -15,7 +15,7 @@ export const LoginComponent = {
                 activationMethod: 'signIn()',
                 content: 'SIGN IN',
                 buttonColorClass:'btn-primary',
-                buttonClasses:'ms-2',
+                buttonClasses:'',
                 spanClasses:'h6',
                 buttonStyle: '',
                 spanStyle: ''
